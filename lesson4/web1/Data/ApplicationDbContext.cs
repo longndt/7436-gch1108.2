1c3f1eabb95b3edb193602df6ee240543e1da80b
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-05-24 09:33:08 +0700

    web

---
+++ lesson4/web1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using web1.Models;
+
+namespace web1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<web1.Models.Laptop> Laptop { get; set; } = default!;
+    }
+}
---
+++ lesson4/web1/Models/Laptop.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web1.Models
+{
+    public class Laptop
+    {
+        //primary key
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Tên tối thiểu 5 ký tự")]
+        [MaxLength(50, ErrorMessage = "Tên tối đa 50 ký tự")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(100, 2000, ErrorMessage = "Giá tiền phải từ 100$ đến 2000$")]
+        public decimal Price { get; set; }
+
+        [StringLength(5000)]
+        [Url]
+        public string Image { get; set; }
+
+        //? : nullable (optional)
+        public string? Description { get; set; }
+    }
+}
