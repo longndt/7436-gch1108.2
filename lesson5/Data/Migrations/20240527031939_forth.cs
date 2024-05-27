c5673cc00d1bc7b985444b26bb5a4a02a7f85be8
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-05-27 10:46:39 +0700

    asp

---
+++ lesson5/Data/Migrations/20240527031939_forth.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace lesson5.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class forth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "role1", null, "Administrator", "ADMINISTRATOR" },
+                    { "role2", null, "User", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "user1", 0, "6a1b89bd-a59f-4932-9427-47ea0eb10b1b", "admin@gmail.com", true, false, null, "ADMIN@GMAIL.COM", "ADMIN@GMAIL.COM", "AQAAAAIAAYagAAAAEDKHucPJDS1LN6YRC40wZvQSLx8erahM0OYXVuEFzcA+pWghJXjeq+ohggYD1/DWEA==", null, false, "08a539af-7ebd-4739-be71-5c6bf26afc3e", false, "admin@gmail.com" },
+                    { "user2", 0, "e2b95a7a-77bb-4da3-b398-b41fd90c1382", "user@gmail.com", true, false, null, "USER@GMAIL.COM", "USER@GMAIL.COM", "AQAAAAIAAYagAAAAEABRSrbH7IQ84fjvvQQ2sf1MGdQPkT8Pq3BdLvwNxUmtTmb5gXH4nPfSNELS8tMCsQ==", null, false, "1baea337-bf53-407c-963e-f8f1d5c86051", false, "user@gmail.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Laptop",
+                columns: new[] { "Id", "Brand", "Color", "Image", "Model", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Apple", "Grey", "https://bizweb.dktcdn.net/100/444/581/products/macbook-m1-vs-intel-1536x1268-6c00654d-ad87-4caf-8b88-aa6c34048199.png?v=1656134590567", "Macbook Pro M2", 2345m, 10 },
+                    { 3, "LG", "White", "https://product.hstatic.net/1000333506/product/pc-gram-17z90q-b-gallery-02_dd780c6249ec430b84f82ed466fffd6e.jpg", "Gram 17", 2024m, 22 },
+                    { 12, "Dell", "Black", "https://thegioiso365.vn/wp-content/uploads/2023/04/Dell-xps-9530-3.png", "XPS15", 1999m, 15 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[,]
+                {
+                    { "role1", "user1" },
+                    { "role2", "user2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "role1", "user1" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "role2", "user2" });
+
+            migrationBuilder.DeleteData(
+                table: "Laptop",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Laptop",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Laptop",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "role1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "role2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "user1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "user2");
+        }
+    }
+}
---
+++ lesson5/Models/Laptop.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lesson5.Models
+{
+    public class Laptop
+    {
+        //primary key
+        [Key]
+        public int Id { get; set; }
+        //public int LaptopId { get; set; }
+
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+        public string Image {  get; set; }
+    }
+}
