c4a18dbc759d0a21cef5986252be83cd3bfe532d
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-06-03 12:28:51 +0700

    demo

---
+++ demo/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using demo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace demo.Controllers
+{
+    public class ProductController : Controller
+    {
+        //declare application db context
+        //use context to manage database
+        private readonly ApplicationDbContext context;
+
+        //declare constructor
+        public ProductController (ApplicationDbContext context)
+        {
+           this.context = context;
+        }
+
+        //READ function (1)
+        //View all products : Product list
+        //SQL: SELECT * FROM Products
+        public IActionResult Index()
+        {
+            //get product data from "Products" table
+            //and save to an array (list)
+            var products = context.Products.ToList();
+
+            //render view along with data
+            return View(products);
+        }
+
+        //READ function (2)
+        //View product by ID : Product Detail
+        //SQL: SELECT * FROM Products WHERE Id = "id"
+        //int? id : id is nullable
+        public IActionResult Detail(int? id)
+        {
+ 
+            return View();
+        }
+    }
+}
---
+++ demo/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace demo.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        //[Required]
+        //[MinLength(3)]
+        //[MaxLength(30)]
+        [StringLength(30, MinimumLength = 3, 
+            ErrorMessage = "Product name must be 3 to 30 characters")]
+        //ErrorMessage: optional => customize message
+        public string Name { get; set; }
+
+        //public double Price { get; set; }
+        [Range(100, 3000, ErrorMessage = "Product price must be from 100$ to 3000$")]
+        public decimal Price { get; set; }
+
+        [Url(ErrorMessage = "Invalid URL")]
+        public string Image { get; set; }
+    }
+}
